1086fada5c42e572ba6112c837cc7a12429f0158
santimc31 <cayetanosantiago63@gmail.com>
2025-04-19 21:08:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const (CosmosClient)=require('@azure/cosmos');
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res)=> {
+
+//Configuracion de Cosmos DB
+const cosmosClient = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
+const database = cosmosClient.database('academic-db');
+const container=database.container('student');
+app.use(express.json());
+
+//Ruta para insertar datos
+app.post('/student',async(req,res)=>(
+    try{
+        const(body)= req;
+        const(resource:createdItem)=await container.items.create(body);
+        res.status(201).send(createdItem);
+    }catch(error){
+    res.status(500).send(error);
+    }    
+));
+
+//Ruta para consultar datos
+app.get('/students',async(req,res)=>(
+    try{
+        const(resources)=await container.items.readAll().fetchAll();
+        res.status(200).send(createdItem);
+    }catch(error){
+        res.status(500).send(error);
+    }    
+));
+
+app.get('/', (req, res) => {
     res.send('Hola estamos en SENATI desplegando nuestro PaaS 2025');
 });
 
+
+
 app.listen(port, () => {
-    console.log('Aplicacion escuchando en el puerto ${port}');
+    console.log('Aplicación escuchando en el puerto ${port}');
 });
